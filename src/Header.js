fd68c507246a3a4c9ed9d7d967531402e53976d5
Kunhi Kasargod <kunhikasargod@Kunhis-MacBook-Air.local>
2023-02-08 19:51:10 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = ({title}) => {
+    /* const headerStyle = {
+        backgroundColor: 'royalblue', 
+        color: '#fff'
+    } */
+  return (
+    <header /* style={headerStyle} */>
+        <h1>{title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: "Default Title"
+}
+
+export default Header
